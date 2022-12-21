fb06e11c0a0b26390852ff540d2132e61e3694d9
eq19 <admin@eq19.com>
2022-12-22 01:47:03 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -46,9 +46,9 @@ export RAILS_VERSION=5.0.1
 export BUNDLER_VER=${BUNDLER_VER}
 export BUNDLE_GEMFILE=/maps/Gemfile
 export BUNDLE_SILENCE_ROOT_WARNING=1
-#export TA_LIBRARY_PATH=${PREFIX}/lib
+export TA_LIBRARY_PATH=${PREFIX}/lib
 export NOKOGIRI_USE_SYSTEM_LIBRARIES=1
-#export TA_INCLUDE_PATH=${PREFIX}/include
+export TA_INCLUDE_PATH=${PREFIX}/include
 export PAGES_REPO_NWO=$GITHUB_REPOSITORY
 export VENDOR_BUNDLE=${WORKING_DIR}/vendor/bundle
 export SSL_CERT_FILE=$(realpath .github/hook-scripts/cacert.pem)
@@ -75,7 +75,7 @@ cd / && pwd && ls -al
 # packages
 echo -e "$hr\nVENV DIR\n$hr"
 cd /maps && pwd && ls -al
-cd ${PREFIX} && ./configure --prefix=/usr && make && make install
+cd ${PREFIX} && ./configure --prefix=${PREFIX} && make && make install
 
 # current
 echo -e "$hr\nWORK DIR\n$hr"
