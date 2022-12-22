064f7431767793ad4897f2e1e2b2f94a9df9cec7
eq19 <admin@eq19.com>
2022-12-22 16:16:19 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -130,8 +130,8 @@ node -v && npm -v
 
 # Restore modification time (mtime) of git files
 echo -e "$hr\nEPOCH TEST\n$hr"
-${SCRIPT_DIR}/init_environment.sh
 ${SCRIPT_DIR}/path_restore.sh
+${SCRIPT_DIR}/init_environment.sh
 ${SCRIPT_DIR}/prime_list.sh
 
 # Clean up bundler cache
